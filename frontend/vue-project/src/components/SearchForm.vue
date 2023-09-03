4bf9ea37904335465ae7da3e165d0f8c3947ddec
pedrosteinmuller <pedrosteinmuller10105@hotmail.com>
2023-09-03 10:57:27 -0300

    feat: organizing files on frontend folder and starting creating initial structure of main page frontend

---
+++ frontend/vue-project/src/components/SearchForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Product Search Engine</h1>
+    <form @submit.prevent="searchProducts">
+      <label for="category">Category:</label>
+      <select v-model="selectedCategory">
+        <option value="Mobile">Mobile</option>
+        <option value="Refrigerator">Refrigerator</option>
+        <option value="TV">TV</option>
+      </select>
+
+      <label for="site">Site:</label>
+      <select v-model="selectedSite">
+        <option value="MercadoLivre">Mercado Livre</option>
+        <option value="Buscape">Buscapé</option>
+      </select>
+
+      <label for="searchText">Search Text:</label>
+      <input type="text" v-model="searchText" />
+
+      <button type="submit">Search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedCategory: 'Mobile',
+      selectedSite: 'MercadoLivre',
+      searchText: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 1.5em;
+  text-align: center;
+  color: white;
+}
+</style>
