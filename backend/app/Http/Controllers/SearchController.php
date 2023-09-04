14f86b5ecfe4d3c43f2990cda3be715f004f581f
pedrosteinmuller <pedrosteinmuller10105@hotmail.com>
2023-09-04 12:51:16 -0300

    feat: creating SearchController to get and save informations to the database, to be continue...

--- backend/app/Http/Controllers/SearchController.php
+++ backend/app/Http/Controllers/SearchController.php
@@ -2,23 +2,99 @@
 
 namespace App\Http\Controllers;
 
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
 use Illuminate\Http\Request;
 use App\Models\SearchResult;
 
 class SearchController extends Controller
 {
-    public function index()
+    public function searchMercadoLivre(Request $request)
     {
-        return view('search.index');
+        $searchText = $request->input('searchText');
+        $results = $this->webScrapMercadoLivre($searchText);
+
+        // Salvando os resultados no banco de dados
+        foreach ($results as $result) {
+            SearchResult::create([
+                'photo' => $result['Photo'],
+                'description' => $result['Description'],
+                'category' => $result['Category'],
+                'price' => $result['Price'],
+                'source_website' => $result['Website'],
+            ]);
+        }
+
+        return response()->json(['products' => $results]);
     }
 
-    public function search(Request $request)
+    public function searchBuscape(Request $request)
     {
-        //lógica de pesquisa e scraping para retornar os resultados em JSON
+        $searchText = $request->input('searchText');
+        $results = $this->webScrapBuscape($searchText);
+
+        foreach ($results as $result) {
+            SearchResult::create([
+                'photo' => $result['Photo'],
+                'description' => $result['Description'],
+                'category' => $result['Category'],
+                'price' => $result['Price'],
+                'source_website' => $result['Website'],
+            ]);
+        }
+
+        return response()->json(['products' => $results]);
     }
 
-    public function saveResult(Request $request)
+    private function webScrapMercadoLivre($searchText)
     {
-        //lógica para salvar resultados no banco de dados
+        $baseUrl = 'https://www.mercadolivre.com.br/';
+        $client = new Client();
+        $crawler = $client->request('GET', $baseUrl . 'search/' . urlencode($searchText));
+
+        $results = $crawler->filter('')->each(function (Crawler $node) {
+            $title = $node->filter('')->text();
+            $price = $node->filter('')->text();
+            $link = $node->filter('')->attr('href');
+            $photo = $node->filter('')->attr('src');
+            $category = 'Mobile';
+
+            return [
+                'Photo' => $photo,
+                'Description' => $title,
+                'Category' => $category,
+                'Price' => $price,
+                'Website' => 'Mercado Livre',
+                'Link' => $link,
+            ];
+        });
+
+        return $results;
+    }
+
+    private function webScrapBuscape($searchText)
+    {
+        $baseUrl = 'https://www.buscape.com.br/';
+        $client = new Client();
+        $crawler = $client->request('GET', $baseUrl . 'search/' . urlencode($searchText));
+
+        $results = $crawler->filter('')->each(function (Crawler $node) {
+            $title = $node->filter('')->text();
+            $price = $node->filter('')->text();
+            $link = $node->filter('')->attr('href');
+            $photo = $node->filter('')->attr('src');
+            $category = 'Mobile';
+
+            return [
+                'Photo' => $photo,
+                'Description' => $title,
+                'Category' => $category,
+                'Price' => $price,
+                'Website' => 'Buscapé',
+                'Link' => $link,
+            ];
+        });
+
+        return $results;
     }
 }
