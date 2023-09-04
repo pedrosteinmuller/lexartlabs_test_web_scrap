a3ff561d1767da1986dd4ded1ffcfec5ec90cbfe
pedrosteinmuller <pedrosteinmuller10105@hotmail.com>
2023-09-04 10:13:02 -0300

    feat: creating initial structure SearchResults form

---
+++ frontend/vue-project/src/components/SearchResults.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Search Results</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [], //resultados da pesquisa recebidos do backend
+    };
+  },
+};
+</script>
+
+<style scoped>
+.product {
+  display: flex;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+.product img {
+  max-width: 100px;
+  max-height: 100px;
+  margin-right: 10px;
+}
+</style>
